63f80f700553af80580d1aa8505624c504d9348d
1n1tial <dan.yun0821@gmail.com>
2025-05-10 21:35:46 +0900

    welcome screen fix

--- src/routes/welcome.tsx
+++ src/routes/welcome.tsx
@@ -1,6 +1,8 @@
 import React, { useRef, useState } from "react";
-import { Box, Button, Input, VStack, Text, Heading } from "@chakra-ui/react";
+import { Box, Button, Input, VStack, Text, HStack, Image, Heading } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
+import logoIcon from "../assets/output.png";
+
 
 const SnapScrollComponent: React.FC = () => {
   const containerRef = useRef<HTMLDivElement>(null);
@@ -39,9 +41,20 @@ const SnapScrollComponent: React.FC = () => {
         flexDirection="column"
         justifyContent="center"
         alignItems="center"
+        pb={24}
         bg="blue.100"
+        position="relative"
       >
-        <Heading>Welcome to My Page</Heading>
+        <Box position="absolute" top={24}>
+          <HStack spacing={3} justifyContent="center">
+            <Image src={logoIcon} alt="Logo" boxSize={12} />
+            <Heading fontSize="4xl" fontWeight="bold">숨: 고르기</Heading>
+          </HStack>
+          <Text mt={5} fontSize="lg" textAlign="center">
+            <Text as="span" fontWeight="bold">그대</Text>와 <Text as="span" fontWeight="bold" color="purple.500">호흡</Text>이 얼마나 잘 맞을까...?
+          </Text>
+        </Box>
+
         <Button onClick={handleScroll} colorScheme="blue">
           Get Started
         </Button>
