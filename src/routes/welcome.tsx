b87d60c017cbf51951317cefb005f0b728e544c3
HK Lee <69958270+tteokgook1g@users.noreply.github.com>
2025-05-10 20:50:16 +0900

    aasdfds

--- src/routes/welcome.tsx
+++ src/routes/welcome.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Box, Button, Heading, Input, VStack } from "@chakra-ui/react";
+import { Box, Button, Center, Heading, Input, VStack } from "@chakra-ui/react";
 
 
 const WelcomeScreen: React.FC = () => {
@@ -14,22 +14,28 @@ const WelcomeScreen: React.FC = () => {
   };
 
   return (
-    <Box h="100vh" w="100vw" display="flex" alignItems="center" justifyContent="center">
-      <VStack spacing={6}>
-        <Heading size="lg">Welcome</Heading>
-        <Input
-          placeholder="Enter your name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          size="md"
-          textAlign="center"
-          w="16rem"
-        />
-        <Button colorScheme="blue" onClick={handleSubmit}>
-          Continue
-        </Button>
-      </VStack>
-    </Box>
+    <VStack h="200vh" w="100vw" overflow={'hidden'}>
+      <Center h="100vh" w="100%">
+        <Heading>Lovepower Calculator</Heading>
+      </Center>
+
+      <Center h="100vh" w="100%">
+        <VStack spacing={6}>
+          <Heading size="lg">Welcome</Heading>
+          <Input
+            placeholder="Enter your name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            size="md"
+            textAlign="center"
+            w="16rem"
+          />
+          <Button colorScheme="blue" onClick={handleSubmit}>
+            Continue
+          </Button>
+        </VStack>
+      </Center>
+    </VStack>
   );
 };
 
